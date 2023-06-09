cc6d1f02e8ef403b2eb65749d16604c6588371df
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-06-09 19:49:40 +0600

    Create 01_Dart_Output.dart

---
+++ 01_Dart_Output.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  print('Hello World!');
+
+  print("Hi, this is Zaber's learning to code in Dart!");
+
+  print('''
+  
+        This is 
+        a multiline 
+        string 
+        output.
+        
+        ''');
+
+  print('25+5');
+  print(25+5);
+
+  print('25+5 = ${25+5}');
+  
+}
